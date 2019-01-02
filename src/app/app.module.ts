e734b75624dfb60d52674da9d707fcbec4415567
debroniswarren <debronisw@gmail.com>
2019-01-02 15:58:56 -0700

    Added navbar to portfolio, buttons are funtional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    GalleryComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
