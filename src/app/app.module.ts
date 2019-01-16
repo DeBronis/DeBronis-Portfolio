0968cff2b35ee1b7f910b7fb2dd826ea1569b2e3
debroniswarren <debronisw@gmail.com>
2019-01-16 08:50:13 -0700

    Added 2nd image for about section. Added parallax scrolling to website.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { AboutComponent } from './about/about.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { AboutComponent } from "./about/about.component";
+import { GalleryComponent } from "./gallery/gallery.component";
+import { ContactComponent } from "./contact/contact.component";
+import { HomeComponent } from "./home/home.component";
+import { ParallaxDirective } from "./parallax.directive";
 
 @NgModule({
   declarations: [
@@ -16,13 +17,11 @@ import { HomeComponent } from './home/home.component';
     AboutComponent,
     GalleryComponent,
     ContactComponent,
-    HomeComponent
+    HomeComponent,
+    ParallaxDirective
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
